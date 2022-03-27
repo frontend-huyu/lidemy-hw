722b868059c32b4e3c4d9c9d240ae5a05e5ce92d
frontend-huyu <yu.huang0206@gmail.com>
2022-03-27 17:07:30 +0800

    Update function.js

---
+++ mentorlesson/lesson1/function.js
@@ -0,0 +1,40 @@
+// return value
+function add1(a, b) {
+  return a + b
+}
+console.log(add1(1, 2)) // 3
+
+// callback case1
+function add2(a, b) {
+  console.log(a + b)
+}
+add2(1, 2) // 3
+
+// callback case2
+function add3(a, b) {
+  return a + b
+}
+add3(1, 2) // nothing happened, function just be done and return value, but there is nothing to catch the value
+
+// put function into variable and declare
+var add4 = function (a, b) {
+  return a + b
+}
+
+
+// take function like variable to put another function into itself
+// it can be anonymous function or not
+var hello = function () {
+  console.log('hellow, wrold!')
+}
+
+function exec(fn) {
+  fn()
+}
+
+exec(hello) // hellow, wrold!
+
+// it also can put function straightly
+exec(function () {
+  console.log(123)
+})
