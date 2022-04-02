075723942a9fbe1afcab6a82e55e0bf38e22c98a
frontend-huyu <yu.huang0206@gmail.com>
2022-04-02 20:37:29 +0800

    Update [JS101] level3

---
+++ week2/[JS101]/practices/level3/practice1_sort.js
@@ -0,0 +1,32 @@
+function sort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] <= arr[j]) {
+        continue
+      } else if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+      }
+    }
+  }
+  // console.log(arr)
+  return arr
+}
+
+sort([6, 8, 3, 2])
+sort([1, 2, 7, 5])
+
+// sort([1, 2, 1, 2, 3, 6, 6, 3])
+
+// 6
+// 6832
+// 3862
+// 3826
+
+// 8
+// 3286
+// 3268
+
+// 6
+// 3268
+
+// 8
---
+++ week2/[JS101]/practices/level3/practice2_flat_array.js
@@ -0,0 +1,46 @@
+let newArr = []
+
+function flatArr(arr) {
+  // if it's an array, callback & return
+  // if arr is not an array, push into newArr
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'object') {
+      flatArr(arr[i])
+    } else {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+}
+
+function flatten(arr) {
+  flatArr(arr)
+  return newArr
+}
+
+// flatten([1, 2, 3])
+// flatten([1, 2, [1, 2], [1, 3], 6])
+flatten([1, [2, [3, [4]], 5], 6])
+
+
+
+// solution takes a cue from other people
+
+// function flatten(arr) {
+//   let result = [];
+//   recursive(result, arr);
+//   return result;
+// }
+
+// function recursive(result, arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] !== "object") {
+//       result.push(arr[i]);
+//     } else {
+//       recursive(result, arr[i]);
+//     }
+//   }
+// }
+// console.log(flatten([1, 2, 3]));
+// console.log(flatten([1, 2, [1, 2], [1, 3], 6]));
+// console.log(flatten([1, [2, [3, [4]], 5], 6]))
